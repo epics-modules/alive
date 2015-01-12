39cf1e35296b08d243e61b28a6085c314fb1bb52
dohnarms <dohnarms@anl.gov>
2015-01-12 21:39:02 +0000

    oops

--- iocBoot/iocaliveEx/st.cmd
+++ iocBoot/iocaliveEx/st.cmd
@@ -14,8 +14,7 @@ dbLoadDatabase "dbd/aliveEx.dbd"
 aliveEx_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
-#dbLoadRecords "db/alive.db", "P=aliveEx:,RHOST=164.54.53.173"
-dbLoadRecords "db/alive.db", "P=aliveEx:,RHOST=164.54.100.11"
+dbLoadRecords "db/alive.db", "P=aliveEx:,RHOST=x.x.x.x"
 
 
 ## Run this to trace the stages of iocInit
