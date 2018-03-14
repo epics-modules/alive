32325e0076b6c61c60ed215cb93fe76b3b5236af
Dohn Arms <dohnarms@anl.gov>
2018-03-14 12:15:29 -0500

    added aliveMSGCalc database

--- iocBoot/iocaliveEx/st.cmd
+++ iocBoot/iocaliveEx/st.cmd
@@ -20,12 +20,15 @@ aliveEx_registerRecordDeviceDriver pdbbase
 
 ## Load record examples (look in alive.db for more settable options)
 #    simplest example
-dbLoadRecords "db/alive.db", "P=aliveEx:,RHOST=$(ALIVE_SERVER)"
+dbLoadRecords("db/alive.db", "P=aliveEx:,RHOST=$(ALIVE_SERVER)" )
 #    if you want to directly name an IOC
-# dbLoadRecords "db/alive.db", "P=aliveEx:,RHOST=$(ALIVE_SERVER),IOCNM=iocCryo"
+# dbLoadRecords("db/alive.db", "P=aliveEx:,RHOST=$(ALIVE_SERVER),IOCNM=iocLab")
 #    if you want to use a custom environment variable, such as IOCVAR
-# dbLoadRecords "db/alive.db", "P=aliveEx:,RHOST=$(ALIVE_SERVER),NMVAR=IOCVAR"
+# dbLoadRecords("db/alive.db", "P=aliveEx:,RHOST=$(ALIVE_SERVER),NMVAR=IOCVAR")
 
+## If you want to set up a calcout to automatically write to the MSG field
+#    based on PV fieldsvalues
+# dbLoadRecords("db/aliveMSGCalc.db", "P=aliveEx:" )
 
 
 ## Run this to trace the stages of iocInit
