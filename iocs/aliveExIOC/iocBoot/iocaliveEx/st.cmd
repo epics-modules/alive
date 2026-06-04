b5f5846ab4cdf98b0b5d8d576c086b3533ec2c17
keenanlang <klang@aps.anl.gov>
2026-06-04 14:30:16 -0500

    Set up automatic dependencies

--- iocs/aliveExIOC/iocBoot/iocaliveEx/st.cmd
+++ iocs/aliveExIOC/iocBoot/iocaliveEx/st.cmd
@@ -1,4 +1,4 @@
-#!../../bin/linux-x86_64/aliveEx
+#!../../bin/linux-x86_64/aliveExApp
 
 ## You may have to change dsh to something else
 ## everywhere it appears in this file
@@ -15,23 +15,22 @@ epicsEnvSet("ALIVE_SERVER","xxx.aps.anl.gov")
 cd ${TOP}
 
 ## Register all support components
-dbLoadDatabase "dbd/aliveEx.dbd"
-aliveEx_registerRecordDeviceDriver pdbbase
+dbLoadDatabase "dbd/aliveExApp.dbd"
+aliveExApp_registerRecordDeviceDriver pdbbase
 
 
 ## Load record examples (look in alive.db for more settable options)
 
 #    if you don't specify IOCNM, the name defaults to the value of the IOC
 #       environment variable, which is explicitly done here
-dbLoadRecords("db/alive.db", "P=aliveEx:,IOCNM=$(IOC),RHOST=$(ALIVE_SERVER)" )
+dbLoadRecords("$(ALIVE)/db/alive.db", "P=aliveEx:,IOCNM=$(IOC),RHOST=$(ALIVE_SERVER)" )
 
 ## If you want to set up a calcout to automatically write to the MSG field
 #    based on PV values
-# dbLoadRecords("db/aliveMSGCalc.db", "P=aliveEx:" )
+# dbLoadRecords("$(ALIVE)/db/aliveMSGCalc.db", "P=aliveEx:" )
 
 
 ## Run this to trace the stages of iocInit
 #traceIocInit
 cd ${TOP}/iocBoot/${IOC}
 iocInit
-
